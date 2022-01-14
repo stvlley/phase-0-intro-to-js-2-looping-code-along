044b65d1ca826125fd03501f96a6880e34e3bc22
stephen talley <stvlley.amzn@gmail.com>
2022-01-14 12:04:54 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(names, str) {
+    const thankYou = []
+    for(let i = 0; i < names.length; i++) {
+    thankYou.push(`Thank you, ${names[i]}, for the wonderful ${str} gift!`)
+    }
+    return thankYou;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown() {
+    let i = 10
+    while ( i > -1) {
+        console.log(i--);
+    }
+}
